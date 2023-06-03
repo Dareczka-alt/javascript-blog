82143920b0c973a80b0be99031b8e42e8f39e62c
Daria Dorna <perelka4@onet.pl>
2023-06-03 12:15:41 +0200

    Ending function generateTitleLinks

--- js/script.js
+++ js/script.js
@@ -44,11 +44,7 @@ function titleClickHandler(event) {
 
   targetArticle.classList.add('active');
 
-  const links = document.querySelectorAll('.titles a');
 
-  for (let link of links) {
-    link.addEventListener('click', titleClickHandler);
-  }
 }
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
@@ -60,7 +56,42 @@ function generateTitleLinks() {
   /* remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
+
+  /* find all the articles and save them to variable: articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html = '';
+
+  for (let article of articles) {
+
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+    console.log(articleId);
+    /* find the title element */
+    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    console.log(articleTitle);
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+
+    /* insert link into titleList */
+    titleList.insertAdjacentHTML('beforeEnd', linkHTML);
+    console.log(titleList);
+
+    /* insert link into html variable */
+    html = html + linkHTML;
+    console.log(html);
+  }
+
+  const links = document.querySelectorAll('.titles a');
+
+  for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+  }
 }
+
 generateTitleLinks();
 
 
